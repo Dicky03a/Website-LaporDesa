21206e8e7d285fc9a60b235c76159a3f5a7c335d
Dicky03a <dickycoding@gmail.com>
2025-10-14 08:43:08 +0700

    Kedua

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 </head>
 
 <body>
-    <h1 class="text-4xl">Hallo Ini projek website kelompoknya <section class="text-8xl"> mas zaki ganteng banget </section> awok awok</h1>
+    <h1 class="text-4xl">Hallo Ini projek website kelompoknya <section class="text-8xl"> mas zaki ganteng banget dan kambali si cool banget anjay</section> awok awok</h1>
 
     <button class="bg-blue-500 text-white px-4 py-2 rounded">
         <a href="{{ route('lapor.create') }}">Klik Saya kalau ada masalah</a>
